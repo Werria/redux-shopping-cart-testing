7cdcf6f65751f9b6f32a78ccf334c00be455e9ee
Weria Heidary <verya.heydari@gmail.com>
2022-12-02 21:08:26 +0330

    test products slice, more scenarios

--- src/features/products/productsSlice.test.ts
+++ src/features/products/productsSlice.test.ts
@@ -17,4 +17,24 @@ describe("products slice", () => {
       expect(result.products[product.id]).toEqual(product);
     });
   });
+  it("should now allow the same product to be added more than once", () => {
+    const initialState = undefined;
+    const action = receivedProducts(products);
+    let result = productsReducer(initialState, action);
+    expect(Object.keys(result.products).length).toEqual(products.length);
+    products.forEach((product) => {
+      expect(result.products[product.id]).toEqual(product);
+    });
+    result = productsReducer(result, action);
+    expect(Object.keys(result.products).length).toEqual(products.length);
+  });
+  it("should allow multiple products to be reveived at different times", () => {
+    const initialState = undefined;
+    const action = receivedProducts(products.slice(0, 2));
+    let result = productsReducer(initialState, action);
+    expect(Object.keys(result.products).length).toEqual(2);
+    const secondAction = receivedProducts(products.slice(2, 4));
+    result = productsReducer(result, secondAction);
+    expect(Object.keys(result.products).length).toEqual(4);
+  });
 });
