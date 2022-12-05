c7a0114b4b10015c733b9723385fef9a026f2af6
Weria Heidary <verya.heydari@gmail.com>
2022-12-05 09:36:28 +0330

    test getMemoizedNumItems selector

--- src/features/cart/cartSlice.test.ts
+++ src/features/cart/cartSlice.test.ts
@@ -2,6 +2,7 @@ import { RootState } from "../../app/store";
 import cartReducer, {
   addToCart,
   CartState,
+  getMemoizedNumItems,
   getNumItems,
   removeFromCart,
   updateQuantity,
@@ -53,6 +54,56 @@ describe("selectors", () => {
       };
       const result = getNumItems({ cart } as RootState);
       expect(result).toEqual(6);
+    });
+  });
+
+  describe("getMemoizedNumItems", () => {
+    it("should return 0 with no items", () => {
+      const cart: CartState = {
+        checkoutState: "READY",
+        errorMessage: "",
+        items: {},
+      };
+      const result = getMemoizedNumItems({ cart } as RootState);
+      expect(result).toEqual(0);
+    });
+    it("should add up the total", () => {
+      const cart: CartState = {
+        checkoutState: "READY",
+        errorMessage: "",
+        items: { abc: 3, def: 3 },
+      };
+      const result = getMemoizedNumItems({ cart } as RootState);
+      expect(result).toEqual(6);
+    });
+    test("should not compute again with same state", () => {
+      const cart: CartState = {
+        checkoutState: "READY",
+        errorMessage: "",
+        items: { abc: 3, def: 3 },
+      };
+      getMemoizedNumItems.resetRecomputations();
+      getMemoizedNumItems({ cart } as RootState);
+      expect(getMemoizedNumItems.recomputations()).toEqual(1);
+      getMemoizedNumItems({ cart } as RootState);
+      expect(getMemoizedNumItems.recomputations()).toEqual(1);
+      getMemoizedNumItems({ cart } as RootState);
+      getMemoizedNumItems({ cart } as RootState);
+      getMemoizedNumItems({ cart } as RootState);
+      expect(getMemoizedNumItems.recomputations()).toEqual(1);
+    });
+    test("should recompute with new state", () => {
+      const cart: CartState = {
+        checkoutState: "READY",
+        errorMessage: "",
+        items: { abc: 3, def: 3 },
+      };
+      getMemoizedNumItems.resetRecomputations();
+      getMemoizedNumItems({ cart } as RootState);
+      expect(getMemoizedNumItems.recomputations()).toEqual(1);
+      cart.items = { abc: 2 };
+      getMemoizedNumItems({ cart } as RootState);
+      expect(getMemoizedNumItems.recomputations()).toEqual(2);
     });
   });
 });
