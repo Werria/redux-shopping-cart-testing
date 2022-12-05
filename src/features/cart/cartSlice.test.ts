5e2c1728b88b3f1fd8cf8934e5dd3652104d65b8
Weria Heidary <verya.heydari@gmail.com>
2022-12-05 09:14:34 +0330

    test getNumItems selector

--- src/features/cart/cartSlice.test.ts
+++ src/features/cart/cartSlice.test.ts
@@ -1,5 +1,8 @@
+import { RootState } from "../../app/store";
 import cartReducer, {
   addToCart,
+  CartState,
+  getNumItems,
   removeFromCart,
   updateQuantity,
 } from "./cartSlice";
@@ -29,4 +32,27 @@ describe("cart reducer", () => {
   });
   test.todo("removeFromCart");
   test.todo("updateQuantity");
+});
+
+describe("selectors", () => {
+  describe("getNumItems", () => {
+    it("should return 0 with no items", () => {
+      const cart: CartState = {
+        checkoutState: "READY",
+        errorMessage: "",
+        items: {},
+      };
+      const result = getNumItems({ cart } as RootState);
+      expect(result).toEqual(0);
+    });
+    it("should add up the total", () => {
+      const cart: CartState = {
+        checkoutState: "READY",
+        errorMessage: "",
+        items: { abc: 3, def: 3 },
+      };
+      const result = getNumItems({ cart } as RootState);
+      expect(result).toEqual(6);
+    });
+  });
 });
