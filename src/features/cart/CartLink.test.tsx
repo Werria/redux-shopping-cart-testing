d964218bef10bd87c024af1e031c71a144d5495f
Weria Heidary <verya.heydari@gmail.com>
2022-12-07 22:04:03 +0330

    test the correct number of items

--- src/features/cart/CartLink.test.tsx
+++ src/features/cart/CartLink.test.tsx
@@ -3,11 +3,34 @@ import { BrowserRouter as Router } from "react-router-dom";
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import { CartLink } from "./CartLink";
+import { addToCart, updateQuantity, removeFromCart } from "./cartSlice";
 import { store } from "../../app/store";
 
 test("should contain a link", () => {
   renderWithContext(<CartLink />);
   expect(screen.getByRole("link")).toBeInTheDocument();
+});
+
+test("should show test when there are no items", () => {
+  renderWithContext(<CartLink />);
+  const link = screen.getByRole("link");
+  expect(link).toHaveTextContent("Cart");
+  expect(link).not.toHaveTextContent("0");
+  expect(link).not.toHaveTextContent("1");
+});
+
+test("ahould show the correct number of items", () => {
+  store.dispatch(addToCart("testItem"));
+  renderWithContext(<CartLink />);
+  const link = screen.getByRole("link");
+  expect(link).toHaveTextContent("1");
+  store.dispatch(updateQuantity({ id: "testItem", quantity: 5 }));
+  expect(link).toHaveTextContent("5");
+  store.dispatch(addToCart("anotherItem"));
+  expect(link).toHaveTextContent("6");
+  store.dispatch(removeFromCart("testItem"));
+  store.dispatch(removeFromCart("anotherItem"));
+  expect(link).toHaveTextContent("Cart");
 });
 
 function renderWithContext(element: React.ReactElement) {
